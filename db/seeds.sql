924916b20d4728264408b4cc954e6a47d7c8b438
geeuho <geeuho@gmail.com>
2018-03-05 16:19:54 -0800

    better/

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burgers_db;
+
+SELECT * FROM burgers;
+
+INSERT INTO burgers (burger_name) VALUES ("The Ultimate Bacon Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("Triple Bypass Burger");
+INSERT INTO burgers (burger_name) VALUES ("Soy Burger");
